94bbd44ef6e18f98f5d37e56554a4901f9ff6d48
Agustin Ruiz <Agustin.Ruiz@ey.com>
2020-01-20 23:37:32 -0600

    Create collection-preview and collection-item components

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,14 @@
+import React, { memo } from "react";
+import "./collection-item.styles.scss";
+
+const CollectionItem = ({ id, name, price, imageUrl }) => (
+  <div className="collection-item">
+    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
+    </div>
+  </div>
+);
+
+export default memo(CollectionItem);
