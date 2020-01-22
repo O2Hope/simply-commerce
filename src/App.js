1ffad43313e0d85b4f4555c7b6d77c828e3fe889
Agustin Ruiz <Agustin.Ruiz@ey.com>
2020-01-21 23:21:53 -0600

    Add firebase and sign in with google

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component"
 import Header from "./components/header/header.component"
-import SignIn from "./pages/sign-in/sign-in.component"
 import { Switch, Route } from "react-router-dom";
 import SignInPage from "./pages/sign-in/sign-in.component";
 
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ-kCagFQbr0y5uLj-RFuLF1cxW25n67w",
+  authDomain: "simply-db.firebaseapp.com",
+  databaseURL: "https://simply-db.firebaseio.com",
+  projectId: "simply-db",
+  storageBucket: "simply-db.appspot.com",
+  messagingSenderId: "245158110013",
+  appId: "1:245158110013:web:5d71065b7dd79588791b54"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+provider.setCustomParameters({ prompt: "select_account" });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
