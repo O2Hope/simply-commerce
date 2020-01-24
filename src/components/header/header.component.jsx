ba25c28882b62f22d7719a91a9f7f3a639ce4b0b
Agustin Ruiz <Agustin.Ruiz@ey.com>
2020-01-23 23:06:03 -0600

    implement reduc, add reducer and action for user to app and header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,23 +2,36 @@ import React, { memo } from "react";
 import { Link } from "react-router-dom";
 import "./header.styles.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-import {auth} from "../../firebase/firebase.utils";
+import { auth } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
 
-const Header = ({currentUser}) => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link to="/" className="logo-container">
       <Logo className="logo" />
     </Link>
     <div className="options">
-      <Link to="/shop" className="option">SHOP</Link>
-      <Link to="/contact" className="option">CONTACT</Link>
-      {
-        currentUser ?
-        <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div> :
-        <Link className="option" to="/signin">SIGN IN</Link>
-      }
+      <Link to="/shop" className="option">
+        SHOP
+      </Link>
+      <Link to="/contact" className="option">
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
     </div>
   </div>
 );
 
-export default memo(Header);
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(memo(Header));
