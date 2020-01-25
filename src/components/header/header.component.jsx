f2d29eed29533cef157c4e213f0a125fd0854366
Agustin Ruiz <Agustin.Ruiz@ey.com>
2020-01-25 01:21:39 -0600

    Add cart reducer and logic to  toggle cart-dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,8 +5,9 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
 import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, cartIsHidden }) => (
   <div className="header">
     <Link to="/" className="logo-container">
       <Logo className="logo" />
@@ -29,11 +30,13 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon/>
     </div>
+    {!cartIsHidden && <CartDropdown/>}
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(memo(Header));
