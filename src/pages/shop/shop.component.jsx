ffb9df555f99986e84eb59b27249eb3491715b0f
Agustin Ruiz <agusvaldes2@live.com>
2020-01-27 23:58:29 -0600

    Move data to redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,7 @@
 import React from "react";
-import { SHOP_DATA } from "../../constants/shop.data";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component"
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
+import CollectionOverview from "../../components/collections-overview/collections-overview.component";
 
-  render(){
-    const {collections} = this.state
-      return(
-          <div className="shop-page">
-          {collections.map(({id, ...otherCollectionProps}) =>(
-              <CollectionPreview key={id} {...otherCollectionProps}/>
-          ))}
-          </div>
-      )
-  }
-}
+const ShopPage = () => <CollectionOverview />;
 
 export default ShopPage;
